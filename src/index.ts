97d1b247a52a58204bf7bbaa444023ee9b91127c
monazak <monazakout67@gmail.com>
2026-07-20 15:01:47 +0300

    feat: add CLI command registry and login handler

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,49 @@
 import { readConfig, setUser, validateConfig } from "./config";
 
-function main (){
-   
-    console.log("Setting the current user...");
-    setUser('mona')
-    console.log("User updated successfully on disk!\n");
+type CommandHandler = (cmdName: string, ...args: string[]) => void;
 
-    console.log("Reading the configuration back from disk...");
-    const config = readConfig()
-   
-    console.log("Current Configuration Object in Memory:");
-    console.log(config);
+type CommandsRegistry = Record<string, CommandHandler>
+
+function handlerLogin(cmdName: string, ...args: string[]){
+    const username = args[0]
+    if (!username){
+        throw new Error('The login handler expects a single argument, the username')
+    }
+    setUser(username)
+    console.log(`User has been successfully set to: "${username}"`)
+}
+
+
+function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler){
+    registry[cmdName]=handler
+}
+
+function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]){
+    const handler = registry[cmdName]
+    if(!handler){
+        throw new Error (`Unknown command: "${cmdName}"`)
+    }
+    handler(cmdName, ...args);
+}
+
+function main (){
+    const registry: CommandsRegistry = {}
+    registerCommand(registry, 'login', handlerLogin)
+    const CLIArgs = process.argv.slice(2)
+    
+    if(CLIArgs.length < 1){
+        console.log("Error: no enough arguments provided.")
+        process.exit(1)
+    }
+
+    const cmdName = CLIArgs[0]
+    const cmdParams = CLIArgs.slice(1)
+
+    try{
+        runCommand(registry, cmdName, ...cmdParams)
+    }catch(err:any){
+        console.log(`Error: ${err.message}`)
+        process.exit(1)
+    }
 }
 main();
